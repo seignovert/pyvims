0c2bbd6596521c9f8004fc939ebb7da3a93a2cee
Benoit Seignovert <support.git@seignovert.fr>
2018-02-12 15:00:44 +0100

    Rename Nav GeoJson

--- pyvims/__init__.py
+++ pyvims/__init__.py
@@ -8,7 +8,7 @@ from .vims_qub import VIMS_QUB
 from .vims_lbl import VIMS_LBL
 
 from .vims_nav import VIMS_NAV
-from .vims_geojson import VIMS_GEOJSON
+from .vims_nav_geojson import VIMS_GEOJSON
 
 from .vims_quicklook import VIMS_QUICKLOOK
 
