141951d39c1807f6055c076d7c23c997df08e8a3
Benoit Seignovert <univ-nantes@seignovert.fr>
2018-05-03 13:39:03 +0200

    Add MANIFEST and update README

---
+++ README.rst
@@ -0,0 +1,60 @@
+.. warning:: This is an pre-alpha version
+
+PyVIMS
+======
+Python package to manipulate the Cassini VIMS data.
+
+
+Install
+-------
+With ``pip``::
+    pip install pyvims
+
+With the ``source files``::
+
+    git clone https://github.com/seignovert/pyvims.git
+    cd pyvims ; python setup.py install
+
+Testing
+-------
+Check the install::
+
+    >>> from pyvims import VIMS
+
+Examples
+-----
+Download test files (ISIS3 cubes of ``1487096932_1``)::
+
+    wget https://vims.univ-nantes.fr/data/isis/T3/C1487096932_1_vis.cub
+    wget https://vims.univ-nantes.fr/data/isis/T3/C1487096932_1_ir.cub
+    wget https://vims.univ-nantes.fr/data/isis/T3/N1487096932_1_vis.cub
+    wget https://vims.univ-nantes.fr/data/isis/T3/N1487096932_1_ir.cub
+
+To use, simply do::
+
+    >>> from pyvims import VIMS
+    >>> cub = VIMS('1487096932_1')
+
+    >>> cub
+    VIMS cube: 1487096932_1 [ISIS3]
+
+    >>> cub.time
+    '2005-02-14T18:05:00.976500'
+
+    >>> cub.target
+    u'TITAN'
+
+    >>> cub.NS, cub.NL
+    (42, 42)
+
+Dependencies:
+--------------
+- Numpy
+- DateTime
+- CV2
+- Piexif
+- PVL_ (`patched issue #34 <https://github.com/planetarypy/pvl/pull/34>`_)
+- PlanetaryImage
+- SpiceyPy
+
+.. _PVL: https://github.com/seignovert/pvl
--- setup.py
+++ setup.py
@@ -6,13 +6,28 @@ except ImportError:
         " setuptools is not installed on this computer."
         "\nInstall ez_setup ([sudo] pip install ez_setup) and try again.")
 
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
 setup(name='pyvims',
       version='0.1.1',
       description='Python package to manipulate the Cassini VIMS data.',
+      long_description=readme(),
+      classifiers=[
+          'Development Status :: 2 - Pre-Alpha',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 2.7',
+          'Topic:: Scientific/Engineering:: Planetary Sciences',
+          'Topic:: Scientific/Engineering:: Atmospheric Science',
+          'Topic:: Scientific/Engineering:: Astronomy',
+          'Intended Audience :: Science/Research',
+      ],
       url='http://github.com/seignovert/pyvims',
       author='Benoit Seignovert',
       author_email='pyvims@seignovert.fr',
       license='MIT',
+      keywords='nasa cassini vims titan',
       packages=['pyvims'],
       install_requires=[
           'numpy',
@@ -26,4 +41,5 @@ setup(name='pyvims',
           'https://github.com/seignovert/pvl/tarball/master#egg=package-1.0',
           'https://github.com/seignovert/planetaryimage/tarball/master#egg=package-1.0',
       ],
+      include_package_data=True,
       zip_safe=False)
