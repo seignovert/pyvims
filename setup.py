0dc10550fa383c27718c67ca84ccfcbac645a59f
Benoit Seignovert <univ-nantes@seignovert.fr>
2018-05-04 01:12:48 +0200

    Add tox setup

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 # This will try to import setuptools. If not here, it fails with a message
 try:
     from setuptools import setup
@@ -6,41 +8,7 @@ except ImportError:
         " setuptools is not installed on this computer."
         "\nInstall ez_setup ([sudo] pip install ez_setup) and try again.")
 
-def readme():
-    with open('README.rst') as f:
-        return f.read()
-
-setup(name='pyvims',
-      description='Python package to manipulate the Cassini VIMS data.',
-      long_description=readme(),
-      classifiers=[
-          'Development Status :: 3 - Alpha',
-          'License :: OSI Approved :: MIT License',
-          'Programming Language :: Python :: 2.7',
-          'Programming Language :: Python :: 3.6',
-          'Topic :: Scientific/Engineering :: Atmospheric Science',
-          'Topic :: Scientific/Engineering :: Astronomy',
-          'Intended Audience :: Science/Research',
-      ],
-      url='http://github.com/seignovert/pyvims',
-      author='Benoit Seignovert',
-      author_email='pyvims@seignovert.fr',
-      license='MIT',
-      keywords='nasa cassini vims titan',
-      packages=['pyvims'],
-      use_scm_version=True,
-      setup_requires=['setuptools_scm'],
-      install_requires=[
-          'numpy',
-          'datetime',
-          'opencv-python',
-          'piexif',
-          'geojson',
-          'spiceypy',
-      ],
-      dependency_links=[
-          'https://github.com/seignovert/pvl/tarball/master#egg=package-1.0',
-          'https://github.com/seignovert/planetaryimage/tarball/master#egg=package-1.0',
-      ],
-      include_package_data=True,
-      zip_safe=False)
+setup(
+    setup_requires=['pbr>=1.9', 'setuptools>=17.1'],
+    pbr=True,
+)
