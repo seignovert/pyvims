6559fbacdd0f728d12576b00060fb337ae33c7dd
Benoit Seignovert <univ-nantes@seignovert.fr>
2018-07-02 19:15:54 +0200

    Improve ArcMap band description

--- pyvims/geotiff/arcmap.py
+++ pyvims/geotiff/arcmap.py
@@ -8,7 +8,7 @@ class ArcMap(object):
             band = etree.SubElement(self.xml, 'PAMRasterBand')
             band.set('band', str(int(i+1)))
             desc = etree.SubElement(band, 'Description')
-            desc.text = str(w) + ' um'
+            desc.text = 'Band {} / {} um'.format(i+1, w)
 
     def dump(self):
         return etree.tostring(self.xml, pretty_print=True)
