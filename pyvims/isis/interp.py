5bdc568b7e1247042c90197eda5f20c5bce90a97
Benoit Seignovert <benoit.a.seignovert@jpl.nasa.gov>
2019-08-21 09:12:14 -0700

    Move `equi_contour` and `equigrid` to projection module

--- pyvims/isis/interp.py
+++ pyvims/isis/interp.py
@@ -6,7 +6,7 @@ import numpy as np
 
 from scipy.interpolate import griddata
 
-from .projection import ortho_grid
+from .projection import equi_contour, equi_grid, ortho_grid
 
 
 def _linspace(x0, x1, res):
@@ -123,83 +123,6 @@ def ortho_interp(xy, data, res, contour=False, method='cubic'):
     return z, grid, _extent(x, y)
 
 
-def _cross_180(lons, dlon=180):
-    """Find the location of contour segment crossing the change of date meridian.
-
-    Note
-    ----
-    The order is reverse to append new data
-    without modifying the cross index.
-
-    """
-    i = np.arange(np.size(lons) - 1)
-    return i[np.abs(lons[1:] - lons[:-1]) > dlon][::-1]
-
-
-def equi_contour(contour, sc_lat, dlon=180):
-    """Extented contour in equirectangular geometry.
-
-    Parameters
-    ----------
-    contour: np.array
-        Longitude and latitude contour coordinates.
-    sc_lat: float
-        Sub-spacecradt latitude.
-
-    Returns
-    -------
-    np.array
-        Wrapped contour(s) in equirectangular projection.
-
-    """
-    clon, clat = contour
-    pole = 90 * np.sign(sc_lat)
-
-    for i in _cross_180(clon):
-        frac = np.abs(180 - (clon[i] % 360)) / np.abs(clon[i+1] % 360 - clon[i] % 360)
-        edge = 180 * np.sign(clon[i])
-        lat = (clat[i+1] - clat[i]) * frac + clat[i]
-        clon = np.insert(clon, i+1, [edge, edge, -edge, -edge])
-        clat = np.insert(clat, i+1, [lat, pole, pole, lat])
-
-    return clon, clat
-
-
-def _equi_grid(glon, glat, npix=1440):
-    """Create optimize equirectangular grid.
-
-    Parameters
-    ----------
-    glon: np.array
-        Ground longitude.
-    glat: np.array
-        Ground latitude.
-    npix: int, optional
-        Maximum mumber of pixel in X-axis (or half in Y-axis)
-
-    Returns
-    -------
-    (np.array, np.array)
-        Equirectangular grid.
-    list
-        Equirectangular extent for pyplot.
-
-    """
-    x0, y0 = np.floor(np.min([glon, glat], axis=1))
-    x1, y1 = np.ceil(np.max([glon, glat], axis=1))
-
-    pix = (x1 - x0) / npix if x1 - x0 > y1 - y0 else (y1 - y0) / (npix / 2)
-
-    x = np.arange(x0 + .5 * pix, x1, pix)
-    y = np.arange(y0 + .5 * pix, y1, pix)
-
-    X, Y = np.meshgrid(x, y)
-    grid = (X, Y)
-    extent = [x0, x1, y1, y0]
-
-    return grid, extent
-
-
 def equi_interp(xy, data, res, contour, sc, r, npix=1440, method='cubic'):
     """Interpolate data in equirectangular projection.
 
@@ -247,7 +170,7 @@ def equi_interp(xy, data, res, contour, sc, r, npix=1440, method='cubic'):
     ctn = equi_contour((c_lon, c_lat), sc[1])
 
     # Equirectangular grid
-    grid, extent = _equi_grid(*ctn, npix=npix)
+    grid, extent = equi_grid(*ctn, npix=npix)
 
     # Interpolate the equirectangular data with the nearest value
     gz_interp = griddata((glon, glat), gz, grid, method='nearest')
--- pyvims/isis/projection.py
+++ pyvims/isis/projection.py
@@ -86,3 +86,81 @@ def ortho_grid(x, y, lon_0=0, lat_0=0, r=1):
     lon, lat = np.degrees([lambd, phi])
     alt = r * (np.max([np.ones(np.shape(rho)), rho / r], axis=0) - 1)
     return deg180(lon), lat, alt
+
+
+def _cross_180(lons, dlon=180):
+    """Find the location of contour segment crossing the change of date meridian.
+
+    Note
+    ----
+    The order is reverse to append new data
+    without modifying the cross index.
+
+    """
+    i = np.arange(np.size(lons) - 1)
+    return i[np.abs(lons[1:] - lons[:-1]) > dlon][::-1]
+
+
+def equi_contour(contour, sc_lat, dlon=180):
+    """Extented contour in equirectangular geometry.
+
+    Parameters
+    ----------
+    contour: np.array
+        Longitude and latitude contour coordinates.
+    sc_lat: float
+        Sub-spacecradt latitude.
+
+    Returns
+    -------
+    np.array
+        Wrapped contour(s) in equirectangular projection.
+
+    """
+    clon, clat = contour
+    pole = 90 * np.sign(sc_lat)
+
+    for i in _cross_180(clon):
+        frac = np.abs(180 - (clon[i] % 360)) / \
+            np.abs(clon[i+1] % 360 - clon[i] % 360)
+        edge = 180 * np.sign(clon[i])
+        lat = (clat[i+1] - clat[i]) * frac + clat[i]
+        clon = np.insert(clon, i+1, [edge, edge, -edge, -edge])
+        clat = np.insert(clat, i+1, [lat, pole, pole, lat])
+
+    return clon, clat
+
+
+def equi_grid(glon, glat, npix=1440):
+    """Create optimize equirectangular grid.
+
+    Parameters
+    ----------
+    glon: np.array
+        Ground longitude.
+    glat: np.array
+        Ground latitude.
+    npix: int, optional
+        Maximum mumber of pixel in X-axis (or half in Y-axis)
+
+    Returns
+    -------
+    (np.array, np.array)
+        Equirectangular grid.
+    list
+        Equirectangular extent for pyplot.
+
+    """
+    x0, y0 = np.floor(np.min([glon, glat], axis=1))
+    x1, y1 = np.ceil(np.max([glon, glat], axis=1))
+
+    pix = (x1 - x0) / npix if x1 - x0 > y1 - y0 else (y1 - y0) / (npix / 2)
+
+    x = np.arange(x0 + .5 * pix, x1, pix)
+    y = np.arange(y0 + .5 * pix, y1, pix)
+
+    X, Y = np.meshgrid(x, y)
+    grid = (X, Y)
+    extent = [x0, x1, y1, y0]
+
+    return grid, extent
