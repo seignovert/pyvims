d8196b6c06cdf6599525a341a66eff3191a4dac3
Benoit Seignovert <benoit.a.seignovert@jpl.nasa.gov>
2019-09-11 17:42:01 -0700

    Add Titan north pole lakes mask

---
+++ pyvims/isis/titan/lakes.py
@@ -0,0 +1,58 @@
+"""Titan lakes module."""
+
+import os
+
+import numpy as np
+
+from scipy import misc as im
+
+from ..projections.sterographic import stereo_scale, xy as stereo_xy
+from ..vars import ROOT_DATA
+
+
+filename = 'titan_npole_lakes_radar_iss_stereo_60.png'
+lat_min = 60
+thresold = 128
+n_pole = True
+lakes = im.imread(os.path.join(ROOT_DATA, filename)) < thresold
+
+center, scale = stereo_scale(lakes, lat_min, n_pole=n_pole)
+
+
+def xy(lon, lat):
+    """Convert geographic coordinates on the image.
+
+    Parameters
+    ----------
+    lon: float
+        Longitude West (degree).
+    lat: float
+        Latitude North (degree).
+
+    Returns
+    -------
+    float, float
+        X-Y coordinates on the image.
+
+    """
+    return stereo_xy(lon, lat, center=(center, center), scale=scale, n_pole=n_pole)
+
+
+def is_lake(lon, lat):
+    """Convert geographic coordinates on the image.
+
+    Parameters
+    ----------
+    lon: float
+        Longitude West (degree).
+    lat: float
+        Latitude North (degree).
+
+    Returns
+    -------
+    bool
+        ``TRUE`` is the coordinates correspond to a lake.
+
+    """
+    x, y = np.clip(np.round(xy(lon, lat)).astype(np.int16), 0, int(2 * center))
+    return lakes[y, x]
