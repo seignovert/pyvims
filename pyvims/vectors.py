Fix invalid multi dimension angle calculation

pyvims/vectors.py
@@ -238,7 +238,7 @@ def vdot(v1, v2):
         return np.dot(np.transpose(v1), v2)
 
     if np.shape(v1)[1:] == np.shape(v2)[1:]:
-        return np.sum(np.multiply(np.transpose(v1), np.transpose(v2)), axis=1)
+        return np.sum(np.multiply(np.transpose(v1), np.transpose(v2)), axis=-1)
 
     raise ValueError('The two vectors must have the same number of points.')
 
@@ -247,10 +247,10 @@ def angle(v1, v2):
     """Angular separation between two vectors."""
     dot = vdot(hat(v1), hat(v2))
 
-    if np.ndim(dot) == 1 and dot > 1:
+    if np.ndim(dot) == 0 and dot > 1:
         return 0
 
-    if np.ndim(dot) > 1:
+    if np.ndim(dot) > 0:
         dot[dot > 1] = 1
 
     return np.degrees(np.arccos(dot))

tests/test_vectors.py
@@ -1,10 +1,11 @@
 """Test vector module."""
 
 import numpy as np
+from numpy.testing import assert_array_almost_equal as assert_array
 
-from pyvims.vectors import areaquad
+from pyvims.vectors import angle, areaquad, vdot
 
-from pytest import approx
+from pytest import approx, raises
 
 
 def test_areaquad():
@@ -14,3 +15,63 @@ def test_areaquad():
 
     assert areaquad(0, 15, 0, 45) == 0
     assert areaquad(0, 15, 30, 15) == 0
+
+
+def test_vdot():
+    """Test dot product between two vectors."""
+    assert vdot([1, 0, 0], [1, 0, 0]) == 1
+    assert vdot([1, 0, 0], [0, 1, 0]) == 0
+    assert vdot([1, 0, 0], [0, 0, 1]) == 0
+    assert vdot([1, 0, 0], [-1, 0, 0]) == -1
+
+    v1 = np.transpose([[1, 0, 0], [1, 0, 0], [1, 0, 0], [1, 0, 0]])
+    v2 = np.transpose([[1, 0, 0], [0, 1, 0], [0, 0, 1], [-1, 0, 0]])
+
+    assert v1.shape == (3, 4)
+    assert v2.shape == (3, 4)
+
+    assert_array(vdot([1, 0, 0], v2), [1, 0, 0, -1])
+    assert_array(vdot(v2, [1, 0, 0]), [1, 0, 0, -1])
+
+    assert_array(vdot(v1, v2), [1, 0, 0, -1])
+
+    v1 = np.transpose([[[1, 0, 0], [1, 0, 0]], [[1, 0, 0], [1, 0, 0]]])
+    v2 = np.transpose([[[1, 0, 0], [0, 1, 0]], [[-1, 0, 0], [0, 0, 1]]])
+
+    assert v1.shape == (3, 2, 2)
+    assert v2.shape == (3, 2, 2)
+
+    assert_array(vdot(v1, v2), [[1, 0], [-1, 0]])
+
+    with raises(ValueError):
+        _ = vdot([[1, 0, 0]], v1)
+
+
+def test_angle():
+    """Test dot product."""
+    assert angle([1, 0, 0], [1, 0, 0]) == 0
+    assert angle([1, 0, 0], [0, 1, 0]) == 90
+    assert angle([1, 0, 0], [-1, 0, 0]) == 180
+
+    assert angle([1, 0, 0], [2, 0, 0]) == 0
+    assert angle([1, 0, 0], [0, 2, 0]) == 90
+    assert angle([1, 0, 0], [-2, 0, 0]) == 180
+
+    v1 = np.transpose([[1, 0, 0], [1, 0, 0], [1, 0, 0], [1, 0, 0]])
+    v2 = np.transpose([[1, 0, 0], [0, 1, 0], [0, 0, 1], [-1, 0, 0]])
+
+    assert v1.shape == (3, 4)
+    assert v2.shape == (3, 4)
+
+    assert_array(angle([1, 0, 0], v2), [0, 90, 90, 180])
+    assert_array(angle(v2, [1, 0, 0]), [0, 90, 90, 180])
+
+    assert_array(angle(v1, v2), [0, 90, 90, 180])
+
+    v1 = np.transpose([[[1, 0, 0], [1, 0, 0]], [[1, 0, 0], [1, 0, 0]]])
+    v2 = np.transpose([[[1, 0, 0], [0, 1, 0]], [[-1, 0, 0], [0, 0, 1]]])
+
+    assert v1.shape == (3, 2, 2)
+    assert v2.shape == (3, 2, 2)
+
+    assert_array(angle(v1, v2), [[0, 90], [180, 90]])